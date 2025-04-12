6a4ae209de04ab2f628cff393df7ef2bc5a0b082
youssef <youssef.rhandoumi@gmail.com>
2025-04-12 11:28:00 +0100

    Initiated repo

---
+++ README.md
@@ -0,0 +1,1 @@
+This is shell script that would eventually automate some repetitive setup for ubuntu pc at school
---
+++ script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+xrandr -s 2560x1440;
+
+gsettings set org.gnome.desktop.interface icon-theme Win10Sur;
+
+flatpak update com.discordapp.Discord com.visualstudio.code org.mozilla.firefox;
